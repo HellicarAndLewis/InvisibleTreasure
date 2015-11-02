2a040a31b213ee431911ba1568eff273946d5862
James <jgbentley10@gmail.com>
2015-11-02 14:06:30 +0000

    Attempted fix for out of sync count-down timers

--- show/src/Countdown.cpp
+++ show/src/Countdown.cpp
@@ -15,25 +15,25 @@ void Countdown::setup() {
 }
 
 void Countdown::update() {
-//    if(enabled) {
-//        float currentTime = ofGetElapsedTimef();
-//        float diff = currentTime - startTime;
-//        progress = diff;
-//        if(progress >= countdownDuration) {
-//            stop();
-//            ofLogVerbose() << "countdown completed";
-//            int i = 0;
-//            ofNotifyEvent(countdownCompleteEvent, i, this);
-//        }
-//    }
-    if (tween.isRunning() && enabled) {
-        progress = tween.update();
-        if (tween.isCompleted()) {
-            ofLogVerbose() << "countdown tween.isCompleted()";
+    if(enabled) {
+        float currentTime = ofGetElapsedTimef();
+        float diff = currentTime - startTime;
+        progress = countdownDuration - diff;
+        if(progress <= 0) {
+            stop();
+            ofLogVerbose() << "countdown completed";
             int i = 0;
             ofNotifyEvent(countdownCompleteEvent, i, this);
         }
     }
+//    if (tween.isRunning() && enabled) {
+//        progress = tween.update();
+//        if (tween.isCompleted()) {
+//            ofLogVerbose() << "countdown tween.isCompleted()";
+//            int i = 0;
+//            ofNotifyEvent(countdownCompleteEvent, i, this);
+//        }
+//    }
 }
 
 void Countdown::draw() {
@@ -44,10 +44,10 @@ void Countdown::draw() {
 //////////////////////////////////////////////////////////////////////////////////
 void Countdown::start(float duration) {
     enabled = true;
-    tween.setParameters(easinglinear, ofxTween::easeIn, duration, 0, duration*1000, 0);
-    tween.start();
-    //    startTime = ofGetElapsedTimef();
-    //    countdownDuration = duration;
+//    tween.setParameters(easinglinear, ofxTween::easeIn, duration, 0, duration*1000, 0);
+//    tween.start();
+    startTime = ofGetElapsedTimef();
+    countdownDuration = duration;
 }
 void Countdown::stop() {
     progress = 0;
@@ -55,8 +55,8 @@ void Countdown::stop() {
 }
 
 bool Countdown::isComplete() {
-    return (tween.isCompleted() && enabled);
-//    return (progress >= 1 && enabled);
+//    return (tween.isCompleted() && enabled);
+    return (progress <= 0 && enabled);
 }
 //////////////////////////////////////////////////////////////////////////////////
 // protected
